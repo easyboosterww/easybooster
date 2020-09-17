aeb5080c95d8cdfc83d1506ef541181a4f274f42
渣渣辉 <noname@bizpay.net>
2020-09-17 18:03:20 +0800

    .

--- src/config/index.js
+++ src/config/index.js
@@ -4,7 +4,6 @@ import abi from '@/config/abis/abi'
  * 主要配置项
  */
 export default {
-    // mainAddress: '0x61A8794e53077e04d1E6c2C806dE77625B9aD443', // 测试服
     // mainAddress: '0xa6447026c2ac746b2074f53f893999d9294228f9', // 正式服
     // mainAddress: '0xeE85Ea1c9988CB3D47723e4A5b23Cb5d9a2b5720', // 正式服
     mainAddress: "0xE3548Ac55B7dce5A058A8a8fc865ae06c691253B",
