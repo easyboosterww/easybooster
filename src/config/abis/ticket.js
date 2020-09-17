42a25617faf4cd2be370cead4ce884843def180c
渣渣辉 <noname@bizpay.net>
2020-09-17 16:53:26 +0800

    first commit

---
+++ src/config/abis/ticket.js
@@ -0,0 +1,77 @@
+export default [
+	{
+		"inputs": [],
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"inputs": [],
+		"name": "booster",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "kill",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"internalType": "address",
+				"name": "",
+				"type": "address"
+			}
+		],
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "_booster",
+				"type": "address"
+			}
+		],
+		"name": "setEasyBooster",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [
+			{
+				"internalType": "address",
+				"name": "toAddress",
+				"type": "address"
+			}
+		],
+		"name": "take",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"name": "transfer",
+		"outputs": [],
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"stateMutability": "payable",
+		"type": "receive"
+	}
+]
---
+++ src/config/ticket.js
@@ -0,0 +1,6 @@
+import abi from './abis/ticket'
+
+export default {
+    mainAddress: '0xeE85Ea1c9988CB3D47723e4A5b23Cb5d9a2b5720',
+    mainAbi: abi,
+}
